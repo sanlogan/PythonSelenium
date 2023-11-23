7af733874811be6bc4b7a62206e313d12fcab865
sanlogan <132922802+sanlogan@users.noreply.github.com>
2023-11-22 21:07:20 -0500

    Add files via upload

---
+++ selenium project.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Firefox()
+
+driver.get("https://www.automationpanda.com/")
+
+driver.find_element(By.ID,"menu-item-9").click()
+
+print(driver.find_element(By.XPATH,"/html/body/div[1]/div/div[1]/div/article/div/div[1]/div[1]").text)
+
+driver.implicitly_wait(6000)
